7bdfab39f6ed21266882017b9a63deb3ee85b3e8
souksana <souksana.phetdara@gmail.com>
2024-06-18 08:36:00 +0200

    hide connection informations

--- connexion.php
+++ connexion.php
@@ -1,16 +1,10 @@
 <?php
-// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-// $mysqli = mysqli_connect('localhost', 'root', '', 'eat');
-// mysqli_set_charset($mysqli, "utf8");
-
-// $connexion = "mysql:host=mysql-sae4012.alwaysdata.net;port=3306;dbname=sae4012_eat";
-
 // Connexion à la base de données
 $dbname = "sae4012_eat";
-$servername = "mysql-sae4012.alwaysdata.net";
+$servername = "";
 $port = 3306;
-$username = "sae4012";
-$password = "jd_souk_4012";
+$username = "";
+$password = "";
 
 try {
     $mysqli = new mysqli($servername, $username, $password, $dbname, $port);
